70eab4385354146f2a5db51e50e8a61b65bd29b7
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-24 14:35:20 +0200

    1

---
+++ js/test.js
@@ -0,0 +1,8 @@
+"use strict";
+class Prova {
+    constructor(text) {
+        this.text = text;
+    }
+}
+let testo = new Prova("testo di prova!!");
+console.log(testo.text);
---
+++ ts/test.ts
@@ -0,0 +1,12 @@
+class Prova {
+    text: string;
+    constructor(text: string) {
+        this.text = text;
+    }
+}
+
+let testo = new Prova("testo di prova!!");
+// string boolean number any
+let txt: string;
+let arr: string[] = [];
+let arr2: Array<string> = [];
